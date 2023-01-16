68f04fd3ca353e7fcb854a79bc2bb75e625ef0b8
sockki <93645009+sockki@users.noreply.github.com>
2023-01-16 16:39:55 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Compoenents/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
